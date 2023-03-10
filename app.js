1fc8bfa7c63f6ca73b3397c88c48f80c816fdfdb
Maynul-portfolio <studymaynulislam@gmail.com>
2023-03-10 19:23:58 +0600

    End the variable part at 7:23pm night

---
+++ app.js
@@ -0,0 +1,25 @@
+console.log('hello')
+console.log(1234)
+
+// variable
+
+var test = 'This is test';
+console.log(test)
+
+let test2 ='Variables defined with let can not be redeclared.';
+
+console.log(test2);
+
+test2 = 'But valu can change';
+
+console.log(test2)
+
+
+// variable declared with const keyword
+
+const test4 = 'Variables defined with const also can not be redeclared and also cannot decleared';
+
+console.log(test4)
+
+
+
